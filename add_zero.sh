13ab77c6b7fa3061ae828f46f3ffaaae627783e9
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-09-11 10:35:29 +0530

    Update add_zero.sh

--- add_zero.sh
+++ add_zero.sh
@@ -9,7 +9,7 @@ for ((i=0;i<{#array[@]}-2;i++))
 do
 for ((j=$i+1;j<{#array[@]}-1;j++))
 do
-for ((k=$j=1;k<{#array[@]};k++))
+for ((k=$j+1;k<{#array[@]};k++))
 do
 	if (( ${array[i]} + ${array[j]} + ${array[k]} == 0 ))
 	then
