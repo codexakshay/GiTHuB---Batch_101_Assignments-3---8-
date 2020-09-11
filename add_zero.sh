90c9dddffe25e3bcbf5b371f9edff5b3aa2d2965
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-09-11 10:37:02 +0530

    Update add_zero.sh

--- add_zero.sh
+++ add_zero.sh
@@ -1,21 +1,20 @@
-#!/bin/bash -x
 array=(-4 -1 -1 0 1 2 3)
 i=0
 j=0
 k=0
 flag=0
 echo "Triplates Whose Sum Is Zero"
-for ((i=0;i<{#array[@]}-2;i++))
+for ((i=0;i<${#array[@]}-2;i++))
 do
-for ((j=$i+1;j<{#array[@]}-1;j++))
+for ((j=$i+1;j<${#array[@]}-1;j++))
 do
-for ((k=$j+1;k<{#array[@]};k++))
+for ((k=$j+1;k<${#array[@]};k++))
 do
-	if (( ${array[i]} + ${array[j]} + ${array[k]} == 0 ))
-	then
-		echo "[${array[i]},${array[j]},${array[k]}]"
-		flag=1
-	fi
+        if (( ${array[i]} + ${array[j]} + ${array[k]} == 0 ))
+        then
+                echo "[${array[i]},${array[j]},${array[k]}]"
+                flag=1
+        fi
 done
 done
 done
